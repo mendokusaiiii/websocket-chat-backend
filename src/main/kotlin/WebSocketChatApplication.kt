e3855e90d3a91e167ea5c1935d36cac237b82b70
victor.orellana <victor.orellana@gmail.com>
2025-04-17 12:27:13 -0300

    feat: chat application

--- src/main/kotlin/WebSocketChatApplication.kt
+++ src/main/kotlin/WebSocketChatApplication.kt
@@ -1,5 +1,10 @@
 package org.mendo
 
-fun main() {
-    println("Hello World!")
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebSocketChatApplication
+fun main(args: Array<String>) {
+    runApplication<WebSocketChatApplication>(*args)
 }
